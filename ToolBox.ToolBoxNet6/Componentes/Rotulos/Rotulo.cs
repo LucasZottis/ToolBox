59ead0149f106146755ddd9bd9878134e337f0ca
Lucas Zottis <zottis.lucas@hotmail.com>
2022-08-21 05:52:04 -0300

    Implementado componente Rotulo.

---
+++ ToolBox.ToolBoxNet6/Componentes/Rotulos/Rotulo.cs
@@ -0,0 +1,16 @@
+﻿using ToolBox.ToolBoxWinForms.Net6.Componentes.Rotulos.Base;
+
+namespace ToolBox.ToolBoxWinForms.Net6.Componentes.Rotulos
+{
+    [DesignerCategory( "Rotulos" ), ToolboxItem( true )]
+    public class Rotulo : RotuloBase
+    {
+        #region Construtores
+
+        public Rotulo( IContainer container ) : base( container )
+        {
+        }
+
+        #endregion Construtores
+    }
+}
